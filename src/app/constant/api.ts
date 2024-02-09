96e8684157d38c0b8195843a07ba7760bf75d50c
satyam808 <satyam808@gmail.com>
2024-02-09 16:37:49 +0530

    updated api url

--- src/app/constant/api.ts
+++ src/app/constant/api.ts
@@ -1,4 +1,4 @@
-export const PROJECTS_URL : string = "/Project/GetAllProjects"
+export const PROJECTS_URL : string = "/project/GetAllProjects"
 
 export const LOGIN_URL : string = "/Auth/Login"
 export const PROFILE_URL : string = "/User/Profile"
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    API_BASE_URL : "https://styamportfolioapi.azurewebsites.net/",
+    API_BASE_URL : "https://styamportfolioapi.azurewebsites.net/api",
     production : false
 };
